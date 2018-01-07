449fbc46fe6af1c1f39464badb7033743275a7f7
Tess Craig <tessashleycraig89@gmail.com>
2018-01-07 16:20:48 -0600

    Commit 2 - updates starter code

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 /* setting up express */
 const express = require('express');
-const path = require('path');
 const logger = require('morgan');
-const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 
 /* importing routes */
@@ -14,26 +13,23 @@ app.listen(PORT, function() {
   console.log(`listening on port ${PORT}`);
 });
 
+/* setting up views */
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
 /* setting static file */
 app.use('/static', express.static(path.join(__dirname, 'public')));
 /* setting up logging */
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-/* where to find templates */
-app.set('views', path.join(__dirname, 'views'));
-/* what kind of view engine to expect */
-app.set('view engine', 'ejs');
 
 /* setting routes */
 app.get('/', function(req, res) {
   res.render('index', {
-    message: 'Hello world!',
+    message: 'Hello World!',
     documentTitle: 'Ada quotes!!',
     subTitle: 'Read some of the coolest quotes around.',
     showMore: true,
-    quoteAuthors: ['Unknown', 'Yoda', 'CS Lewis', 'Frank Chimero', 'Pablo Picasso', 'Italo Calvino', 'T. S. Eliot', 'Samuel Beckett', 'Hunter S. Thompson', 'another author', 'etc etc'],
+    quoteAuthors: ['Unknown', 'Yoda', 'CS Lewis', 'Frank Chimero', 'Pablo Picasso', 'Italo Calvino', 'T. S. Eliot', 'Samuel Beckett', 'Hunter S. Thompson'],
   });
 });
 app.use('/quotes', quoteRoutes);
