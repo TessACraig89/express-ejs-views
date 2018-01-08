bc7ac3e64f0e4d19c16571aa07c8f7fe31384e9d
Tess Craig <tessashleycraig89@gmail.com>
2018-01-07 20:03:05 -0600

    Commit 7 - fixes rendering quotes array

--- routes/quotes.js
+++ routes/quotes.js
@@ -1,48 +1,22 @@
-//set up express
+/* setting up router */
 const express = require('express');
-
-//create modular, mountable route handlers using express.Router
 const quoteRoutes = express.Router();
 
-//require quotes from database and store in quotesData var
+/* getting quotes from database */
 const quotesData = require('../db/quotes');
 
-//when routes to home pafe
-  //render quotes/quotes-index view, quotesData object, quoteClass, and documentTitle
+/* get all quotes */
 quoteRoutes.get('/', function(req, res) {
   res.render('quotes/quotes-index', {
-    quotes: quotesData,
-    quoteClass: 'my-quote',
-    documentTitle: 'Ada quotes!!'
-  });
-});
-
-//when routed to home page push quotesData
-quoteRoutes.post('/', function(req, res) {
-  quotesData.push({
-    quote: req.body.quote,
-    author: req.body.author,
-    genre: req.body.genre,
-  });
-  res.redirect('/quotes');
-});
-
-//define the add route
-
-quoteRoutes.get('/add', function(req, res) {
-  res.render('quotes/quotes-add', {
     documentTitle: 'Ada\'s Quotes!!',
+    quotesData: quotesData,
   });
 });
 
 /* get individual quote */
 quoteRoutes.get('/:id', function(req, res) {
   const id = req.params.id;
-  res.render('quotes/quotes-single', {
-    documentTitle: 'Ada quotes!!',
-    quoteClass: 'single-quote',
-    quote: quotesData[id]
-  });
+  res.send(quotesData[id]);
 });
 
 /* exporting */
